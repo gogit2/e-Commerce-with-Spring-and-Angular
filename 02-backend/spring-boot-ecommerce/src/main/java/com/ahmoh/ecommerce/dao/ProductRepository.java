b10965191b0676980ff36dde03c8f5fc0565f5ef
gogit2 <gogit2hd@gmail.com>
2022-10-09 02:04:06 +0200

    add search by name method in repo

--- 02-backend/spring-boot-ecommerce/src/main/java/com/ahmoh/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/ahmoh/ecommerce/dao/ProductRepository.java
@@ -12,4 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") long id, Pageable pageable);
 
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
 }
