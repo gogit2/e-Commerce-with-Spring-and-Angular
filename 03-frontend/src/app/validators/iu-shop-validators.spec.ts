118b0f582624a671960c8caacaa93422b6a0ab35
gogit2 <gogit2hd@gmail.com>
2022-11-30 23:55:35 +0200

    Add custom validation class
    add method to validate on white space

---
+++ 03-frontend/src/app/validators/iu-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { IuShopValidators } from './iu-shop-validators';
+
+describe('IuShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new IuShopValidators()).toBeTruthy();
+  });
+});
---
+++ 03-frontend/src/app/validators/iu-shop-validators.ts
@@ -0,0 +1,17 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
+export class IuShopValidators {
+
+    // white space validation
+    static notOnlyWhiteSpace(control: FormControl) : ValidationErrors{
+        if((control.value != null) && (control.value.trim().length === 0)){
+            
+            // invalid, retyrn error object
+            return {'notOnlyWhitespace' : true};
+        } else {
+            // valid, return null
+            return null;
+        }
+    }
+
+}
