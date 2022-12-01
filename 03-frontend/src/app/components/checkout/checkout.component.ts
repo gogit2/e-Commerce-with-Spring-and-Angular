bee90673065cdfe300faa87b16ef806d123fbac3
gogit2 <gogit2hd@gmail.com>
2022-12-01 14:45:09 +0200

    Add validation to billing address form group

--- 03-frontend/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/src/app/components/checkout/checkout.component.ts
@@ -43,11 +43,11 @@ export class CheckoutComponent implements OnInit {
         zipCode: new FormControl('', [Validators.required, Validators.minLength(2), IuShopValidators.notOnlyWhiteSpace])
       }),
       billingAddress: this.formBuilder.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: ['']
+        country: new FormControl('', [Validators.required]),
+        street: new FormControl('', [Validators.required, Validators.minLength(2), IuShopValidators.notOnlyWhiteSpace]),
+        city: new FormControl('', [Validators.required, Validators.minLength(2), IuShopValidators.notOnlyWhiteSpace]),
+        state: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required, Validators.minLength(2), IuShopValidators.notOnlyWhiteSpace])
       }),
       creditCard: this.formBuilder.group({
         cardType: [''],
@@ -149,6 +149,12 @@ export class CheckoutComponent implements OnInit {
   get shippingAddressCity(){ return this.checkoutFormGroup.get('shippingAddress.city') }
   get shippingAddressState(){ return this.checkoutFormGroup.get('shippingAddress.state') }
   get shippingAddressZipCode(){ return this.checkoutFormGroup.get('shippingAddress.zipCode') }
+
+  get billingAddressCountry(){ return this.checkoutFormGroup.get('billingAddress.country') }
+  get billingAddressStreet(){ return this.checkoutFormGroup.get('billingAddress.street') }
+  get billingAddressCity(){ return this.checkoutFormGroup.get('billingAddress.city') }
+  get billingAddressState(){ return this.checkoutFormGroup.get('billingAddress.state') }
+  get billingAddressZipCode(){ return this.checkoutFormGroup.get('billingAddress.zipCode') }
 
   getStates(formGrupName: string){
 
