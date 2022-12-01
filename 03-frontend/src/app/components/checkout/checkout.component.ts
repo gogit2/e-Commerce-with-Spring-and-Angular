741af27fa1617993b1d4b733353abbbc393f2e7e
gogit2 <gogit2hd@gmail.com>
2022-12-01 14:21:49 +0200

    Add validation to shipping address form

--- 03-frontend/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/src/app/components/checkout/checkout.component.ts
@@ -36,11 +36,11 @@ export class CheckoutComponent implements OnInit {
         email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
-        country: [''],
-        street: [''],
-        city: [''],
-        state: [''],
-        zipCode: ['']
+        country: new FormControl('', [Validators.required]),
+        street: new FormControl('', [Validators.required, Validators.minLength(2), IuShopValidators.notOnlyWhiteSpace]),
+        city: new FormControl('', [Validators.required, Validators.minLength(2), IuShopValidators.notOnlyWhiteSpace]),
+        state: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('', [Validators.required, Validators.minLength(2), IuShopValidators.notOnlyWhiteSpace])
       }),
       billingAddress: this.formBuilder.group({
         country: [''],
@@ -143,6 +143,12 @@ export class CheckoutComponent implements OnInit {
   get firstName(){ return this.checkoutFormGroup.get('customer.firstName') }
   get lastName(){ return this.checkoutFormGroup.get('customer.lastName') }
   get email(){ return this.checkoutFormGroup.get('customer.email') }
+  
+  get shippingAddressCountry(){ return this.checkoutFormGroup.get('shippingAddress.country') }
+  get shippingAddressStreet(){ return this.checkoutFormGroup.get('shippingAddress.street') }
+  get shippingAddressCity(){ return this.checkoutFormGroup.get('shippingAddress.city') }
+  get shippingAddressState(){ return this.checkoutFormGroup.get('shippingAddress.state') }
+  get shippingAddressZipCode(){ return this.checkoutFormGroup.get('shippingAddress.zipCode') }
 
   getStates(formGrupName: string){
 
