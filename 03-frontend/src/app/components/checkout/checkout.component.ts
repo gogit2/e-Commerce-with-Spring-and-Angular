ab10951ae342eb7ad071b1bb9297d40c6027bf10
gogit2 <gogit2hd@gmail.com>
2022-12-07 23:59:06 +0200

    Update checkout component
    update onSubmit to place order()

--- 03-frontend/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/src/app/components/checkout/checkout.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { O_TRUNC } from 'constants';
 import { Country } from 'src/app/common/country';
+import { Order } from 'src/app/common/order';
+import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 import { State } from 'src/app/common/state';
 import { CartService } from 'src/app/services/cart.service';
+import { CheckoutService } from 'src/app/services/checkout.service';
 import { CreditCardService } from 'src/app/services/credit-card.service';
 import { IuShopValidators } from 'src/app/validators/iu-shop-validators';
 
@@ -25,7 +31,9 @@ export class CheckoutComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               private creditCardService: CreditCardService, 
-              private cartService: CartService) { }
+              private cartService: CartService,
+              private checkoutService: CheckoutService,
+              private router: Router) { }
 
   ngOnInit(): void {
 
@@ -194,11 +202,68 @@ export class CheckoutComponent implements OnInit {
     if(this.checkoutFormGroup.invalid){
       this.checkoutFormGroup.markAllAsTouched();
     }
-    console.log(this.checkoutFormGroup.get('customer')?.value)
-    console.log("==========================================")
-    console.log(this.checkoutFormGroup.get('shippingAddress')?.value)
-    console.log("==========================================")
-    console.log(this.checkoutFormGroup.get('billingAddress')?.value)
+    
+    // set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
+
+    // get cart items
+    const cartItems = this.cartService.cartItems;
+
+    // create orderItems from cartItems 
+    let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
+
+    // set up purchase
+    let purchase = new Purchase();
+
+    // populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+    // populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingAddressState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    const shippingAddressCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.state = shippingAddressState.name;
+    purchase.shippingAddress.country = shippingAddressCountry.name;
+
+    // populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingAddressState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+    const billingAddressCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingAddressState.name;
+    purchase.billingAddress.country = billingAddressCountry.name;
+
+    // populate purchase - order and order items
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    // call REST API via the checkoutService
+    this.checkoutService.placeOrder(purchase).subscribe({
+      next: response => {
+        alert(`Your order has been received.\nOrder tracking number: ${response.orderTrackingNumber}`);
+
+        // reset cart 
+        this.resetCart();
+      }, 
+      error: err => {
+        alert(`There was an error: ${err.message}`);  
+      } 
+    })
+
+  }
+
+  resetCart() {
+    // reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    // reset the form 
+    this.checkoutFormGroup.reset();
+
+    // navigate back to the products page
+    this.router.navigateByUrl("/products");
   }
     
 }
--- 03-frontend/src/app/services/checkout.service.ts
+++ 03-frontend/src/app/services/checkout.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Purchase } from '../common/purchase';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,8 @@ export class CheckoutService {
   private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
 
   constructor(private httpClient: HttpClient) { }
+
+  placeOrder(purchase: Purchase): Observable<any>{
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
 }
