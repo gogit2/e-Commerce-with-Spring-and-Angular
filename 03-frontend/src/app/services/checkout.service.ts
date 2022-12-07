cf6aaaf7d8fb2236110468d1fef1bac0db5fe61e
gogit2 <gogit2hd@gmail.com>
2022-12-07 23:17:12 +0200

    Add CheckoutService
    Make REST API call to backend

---
+++ 03-frontend/src/app/services/checkout.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+
+  private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
+
+  constructor(private httpClient: HttpClient) { }
+}
